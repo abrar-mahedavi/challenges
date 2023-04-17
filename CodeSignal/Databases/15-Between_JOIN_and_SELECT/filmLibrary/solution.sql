e233086aafdf3b5305cd6b844f1ce91a7516d1de
abrar-mahedavi <mahedavi.abrar@gmail.com>
2023-04-17 20:24:58 +0530

    Solve 15 Between JOIN and SELECT
    - 74 filmLibrary.

---
+++ CodeSignal/Databases/15-Between_JOIN_and_SELECT/filmLibrary/solution.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE solution()
+BEGIN
+	/* Write your SQL here. Terminate each statement with a semicolon. */
+	SET @favGenre = (SELECT genre FROM movies GROUP BY genre ORDER BY COUNT(*) DESC LIMIT 1);
+    SELECT aa.actor as actor, age
+    FROM actor_ages aa INNER JOIN starring_actors sa ON aa.actor = sa.actor
+    INNER JOIN movies m ON sa.movie_name = m.movie
+    WHERE m.genre=@favGenre
+    ORDER BY age DESC, aa.actor;
+END
