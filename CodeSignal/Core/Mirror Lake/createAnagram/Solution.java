f7dee949d7a82cd3926b5e3e2bce9b1179e2c23a
abrar-mahedavi <mahedavi.abrar@gmail.com>
2023-07-06 08:18:36 +0530

    Solve Mirror Lake.
    - 61 createAnagram.

--- CodeSignal/Core/Mirror Lake/createAnagram/Solution.java
+++ CodeSignal/Core/Mirror Lake/createAnagram/Solution.java
@@ -1,15 +1,16 @@
 public class Solution {
 
-  function solution(s, t) {
-    s = s.split("");
-    var pos;
-    for (var i = 0; i < t.length; i++) {
-      pos = s.indexOf(t[i]);
-      if (pos >= 0) {
-        s.splice(pos, 1);
-      }
-    }
-    return s.length;
+  int solution(String s, String t) {
+    int[] count = new int[256];
+    for (byte b : s.getBytes())
+      count[b]++;
+    for (byte b : t.getBytes())
+      count[b]--;
+    int total = 0;
+    for (int i : count)
+      if (i > 0)
+        total += i;
+    return total;
   }
 
 }
